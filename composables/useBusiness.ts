8098d75db8be90d9b5655384c865c3494fff255f
grey-young <1realgrey@gmail.com>
2026-06-10 16:39:30 +0000

    change the call number

--- composables/useBusiness.ts
+++ composables/useBusiness.ts
@@ -13,7 +13,7 @@ export const useBusiness = () => {
     email: "info@campdawnesports.com",
     emailHref: "mailto:info@campdawnesports.com",
 
-    phone: "+233 594388395",
+    phone: "+233 247042495",
     phoneHref: `tel:+${phoneDigits}`,
     whatsappHref: `https://wa.me/${phoneDigits}`,
 
